a4c54abc2fae80d2d729a948c52124267e74247e
devtiagomantay <devtiagomantay@gmail.com>
2023-03-21 23:13:49 +0000

    Add unit tests for logic functions

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # aptoide-cli-store
 Simple assignement to simulate purchases in the aptoide store
+
+
+## Unit tests
+
+Run unit tests
+
+> pytest
+
+### Test coverage
+
+> pytest --cov=.
--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,5 +1,5 @@
 import pytest
-from src.logic import calculate_prices
+from src.logic import calculate_prices, get_app_purchases_by_userid, get_percentage
 from datetime import datetime
 from src.models import Purchase
 from src.models import Item
@@ -9,13 +9,13 @@ from src.models import Item
 def item_list():
 	item_list = [
 		Item(id=1, app_id=1, name='Premium', price=2.0),
-		Item(id=3, app_id=2, name='Premium', price=1.0)]
+		Item(id=3, app_id=2, name='Coin', price=1.0)]
 	yield item_list
 
 
 @pytest.fixture()
 def purchase_list():
-	# user1 = 1 purchase, user2 = 2 purchases, user3 = 10 purchases in the same app
+	# user1 = 1 purchase, user2 = 2 purchases, user3 = 10 purchases on app_id = 1
 	purchase_list = [
 		Purchase(id=1, item_id=1, app_id=1, user_id=1, value=2.0, date=datetime.now(), reward=None),
 		Purchase(id=2, item_id=1, app_id=1, user_id=2, value=2.0, date=datetime.now(), reward=None),
@@ -34,36 +34,54 @@ def purchase_list():
 	yield purchase_list
 
 
-class TestLogic:
-	def test_calculate_prices_without_discount(self, mocker, item_list, purchase_list):
+class TestGetPercentage:
+	def test_get_percentage(self, purchase_list):
+		assert get_percentage(purchase_amount=0) == 100
+		assert get_percentage(purchase_amount=1) == 95
+		assert get_percentage(purchase_amount=2) == 95
+		assert get_percentage(purchase_amount=3) == 95
+		assert get_percentage(purchase_amount=4) == 95
+		assert get_percentage(purchase_amount=10) == 90
+		assert get_percentage(purchase_amount=24) == 90
+
+
+class TestGetAppPurchasesByUserId:
+	def test_get_app_purchases_by_userid(self, purchase_list):
+		assert get_app_purchases_by_userid(app_id=1, user_id=1, purchase_list=purchase_list) == 1
+		assert get_app_purchases_by_userid(app_id=1, user_id=2, purchase_list=purchase_list) == 2
+		assert get_app_purchases_by_userid(app_id=1, user_id=3, purchase_list=purchase_list) == 10
+		assert get_app_purchases_by_userid(app_id=2, user_id=1, purchase_list=purchase_list) == 0
+		assert get_app_purchases_by_userid(app_id=2, user_id=3, purchase_list=purchase_list) == 1
+
+
+class TestLogicCalculatePrices:
+	def test_calculate_prices_without_discount_app1(self, mocker, item_list, purchase_list):
 		mocker.patch("src.logic.get_app_purchases_by_userid", return_value=1)
 		mocker.patch("src.logic.get_percentage", return_value=100)
 		user_id = 1
-		assert calculate_prices(item_list[0], user_id, purchase_list) == {'price': 2.0, 'dev': 1.50, 'store': 0.50,
-																  'rewards': None}, \
-			"Value should be mocked"
+		result = {'price': 2.0, 'dev': 1.50, 'store': 0.50, 'rewards': None}
+		assert calculate_prices(item_list[0], user_id, purchase_list) == result, "Value should be mocked"
 
 	def test_calculate_prices_without_discount_app2(self, mocker, item_list, purchase_list):
 		mocker.patch("src.logic.get_app_purchases_by_userid", return_value=1)
 		mocker.patch("src.logic.get_percentage", return_value=100)
 		user_id = 3
-		assert calculate_prices(item_list[1], user_id, purchase_list) == {'price': 1.0, 'dev': 0.75, 'store': 0.25,
-																  'rewards': None}, \
-			"Value should be mocked"
+		result = {'price': 1.0, 'dev': 0.75, 'store': 0.25, 'rewards': None}
+		assert calculate_prices(item_list[1], user_id, purchase_list) == result, "Value should be mocked"
 
 	def test_calculate_prices_with_5_percent_reward(self, mocker, item_list, purchase_list):
 		mocker.patch("src.logic.get_app_purchases_by_userid", return_value=2)
 		mocker.patch("src.logic.get_percentage", return_value=95)
 		user_id = 2
-		assert calculate_prices(item_list[0], user_id, purchase_list) == {'price': 1.9, 'dev': 1.42, 'store': 0.47,
-																  'rewards': 95}, "Value should be mocked"
+		result = {'price': 1.9, 'dev': 1.42, 'store': 0.47,'rewards': 95}
+		assert calculate_prices(item_list[0], user_id, purchase_list) == result, "Value should be mocked"
 
 	def test_calculate_prices_with_10_percent_reward(self, mocker, item_list, purchase_list):
 		mocker.patch("src.logic.get_app_purchases_by_userid", return_value=10)
 		mocker.patch("src.logic.get_percentage", return_value=90)
 		user_id = 3
-		assert calculate_prices(item_list[0], user_id, purchase_list) == {'price': 1.8, 'dev': 1.35, 'store': 0.45,
-																  'rewards': 90}, "Value should be mocked"
+		result = {'price': 1.8, 'dev': 1.35, 'store': 0.45, 'rewards': 90}
+		assert calculate_prices(item_list[0], user_id, purchase_list) == result, "Value should be mocked"
 
 	def teardown_method(cls):
 		pass
