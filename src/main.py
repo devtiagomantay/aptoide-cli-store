19c1d05925ca25f4957f6c515ba57eef16b7a707
devtiagomantay <devtiagomantay@gmail.com>
2023-03-24 12:22:04 +0000

    Refactor main

--- src/main.py
+++ src/main.py
@@ -1,89 +1,96 @@
-from models import *
-from helper import *
-from logic import calculate_prices
-
-# global variables
-users_list_ = []
-app_list_ = []
-item_list_ = []
-dev_list = []
-purchase_list_ = []
-store = None
+from data import create_dummy_data
+from logic import create_transaction, get_app, get_item, get_user, get_dev_by_appid, calculate_prices
 
 
-# Data creation
-def create_data():
-	global store
-	global users_list_
-	global dev_list
-	global app_list_
-	global item_list_
-	global purchase_list_
-
-	store = Store(id=1, name='Aptoide', balance=10.0)
-	user1 = User(id=1, name='Bob', balance=100.0)
-	user2 = User(id=2, name='Bill', balance=10.0)
-	dev1 = Dev(id=1, name='Billy', balance=10.0)
-	dev2 = Dev(id=2, name='Joe', balance=10.0)
-	app1 = App(id=1, dev_id=1, name='Chess')
-	app2 = App(id=2, dev_id=1, name='Notepad')
-	item1 = Item(id=1, app_id=1, name='Premium', price=2.0)
-	item2 = Item(id=2, app_id=2, name='Basic', price=1.0)
-
-	users_list_ = [user1, user2]
-	app_list_ = [app1, app2]
-	item_list_.append(item1)
-	item_list_.append(item2)
-	dev_list = [dev1, dev2]
-
-
-def purchase(pi):
+def get_user_input():
 	"""
-	Calculates the final price with or without reward of the app and the share of the developer and store
-	:param pi: dict
-	:return: obj class Purchase
+	Function to get the user input and validate
+	:return: dict
 	"""
-	try:
-		app = search_app(pi['app'], app_list_)
-		item = get_item(pi['item'], item_list_)
-		user = get_user(pi['user'], users_list_)
-		dev = get_dev_by_appid(app.id, app_list_, dev_list)
+	input_ = input('Enter the app, item and user: ')
+	input_list = input_.split()
+	if len(input_list) != 3:
+		print('Invalid values')
+		raise
 
-		values = calculate_prices(item, dev.id, purchase_list_)
+	return {'app': input_list[0], 'item': input_list[1], 'user': input_list[2]}
 
-		user.purchase_app(values['price'])
-		store.sell_item(values['store'])
-		dev.sell_item(values['dev'])
 
-		purchase = Purchase(id=len(purchase_list_) + 1,
-							item_id=item.id,
-							app_id=app.id,
-							user_id=user.id,
-							value=values['price'],
-							date=datetime.now(),
-							reward=values['rewards'])
-		purchase_list_.append(purchase)
-		print(purchase)
+def float_formatter(x):
+	return "%.2f" % x
 
-	except Exception as e:
-		# log error
-		pass
+
+def print_purchase_transaction(purchase, app, item, dev, user, purchase_values):
+	brackets = '{'
+	brackets_end = '}'
+	print(f'PURCHASE TRANSACTION => id: {purchase.id}; app: {app.name}; item: {item.name}; '
+		  f'amount: €{float_formatter(purchase_values["price"])}; '
+		  f'sender: {user.name}; '
+		  f'receivers: {brackets}{dev.name}: €{float_formatter(purchase_values["dev"])}; '
+		  f'{store.name}: €{float_formatter(purchase_values["store"])}{brackets_end}')
+
+
+def print_balance(info):
+	output = f'BALANCE => {info["user"].name}: €{float_formatter(info["user"].balance)}; '
+	if 'dev' in info.keys():
+		output += f'{info["dev"].name}: €{float_formatter(info["dev"].balance)}; '
+	output += f'{info["store"].name}: €{float_formatter(info["store"].balance)}'
+	print(output)
+
+
+def print_reward_transaction(info):
+	brackets = '{'
+	brackets_end = '}'
+	print(f'REWARD TRANSACTION => id: {info["reward_transaction"].id}; '
+		  f'amount: €{float_formatter(info["reward_transaction"].value)}; '
+		  f'sender: {info["reward_transaction"].store}; '
+		  f'receivers: {brackets}{info["user"].name}: €{float_formatter(info["reward_transaction"].value)}{brackets_end}')
 
 
 if __name__ == '__main__':
-	create_data()
+	data_ = create_dummy_data()
 	while 1:
 		try:
-			user_input = input('Enter the app, item and user: ')
-			a = user_input.split()
-			# TODO: input validation
-			purchase_info = {'app': a[0], 'item': a[1], 'user': a[2]}
-			purchase(purchase_info)
-			print('\n purchase_list_: ', len(purchase_list_))
-			print('Developers:', dev_list)
-			print('Store: ', store)
-			print('Users:', users_list_)
-			print('Purchases: ', purchase_list_)
-			print('\n ------------- Press ctrl + C to exit -------------\n\n')
+			user_input = get_user_input()
+
+			app = get_app(user_input['app'], data_['apps'])
+			item = get_item(user_input['item'], data_['items'])
+			user = get_user(user_input['user'], data_['users'])
+			dev = get_dev_by_appid(app.id, data_['apps'], data_['devs'])
+			store = data_['store']
+
+			purchase_values = calculate_prices(item, dev.id, data_['transactions'])
+
+			user.purchase_app(purchase_values['price'])
+			store.sell_item(purchase_values['store'])
+			dev.sell_item(purchase_values['dev'])
+
+			pi = {'purchase_values': purchase_values, 'purchase_total': len(data_['transactions']), 'item_id': item.id,
+				  'app_id': app.id, 'user_id': user.id, 'store': store.name}
+
+			purchase = create_transaction(pi)
+			data_['transactions'].append(purchase)
+
+			print_purchase_transaction(purchase, app, item, dev, user, purchase_values)
+			print_balance({'user': user, 'dev': dev, 'store': store})
+
+			if purchase_values['rewards']:
+				discount = purchase_values['price'] * purchase_values['rewards']
+				user.get_reward(discount)
+				store.give_reward(discount)
+				purchase_values['price'] = discount
+				pi = {'purchase_values': purchase_values, 'purchase_total': len(data_['transactions']),
+					  'item_id': item.id,
+					  'app_id': app.id, 'user_id': user.id, 'store': store.name}
+
+				reward_transaction = create_transaction(pi)
+				data_['transactions'].append(reward_transaction)
+
+			if purchase_values['rewards']:
+				print('#########')
+				print_reward_transaction({'transaction': reward_transaction, 'user': user})
+				print_balance({'user': user, 'store': store})
+
 		except Exception as e:
+			print(e)
 			continue
